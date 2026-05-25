2361ed1ce81c1f2a5b5066f1389c49598d16e089
Catzilla016 <alexiosg555@gmail.com>
2026-05-25 11:58:07 -0600

    Spelling check included!

--- game/Game_Files/gameMode1.py
+++ game/Game_Files/gameMode1.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import json
+import difflib
 
 class gameMode1():
     def __init__(self):
@@ -8,17 +9,217 @@ class gameMode1():
         self.root = tk.Tk()
         self.button = tk.Button(self.root, text="Submit", width=25, command=self.on_button_click)
         self.entry = tk.Entry(self.root)
+        self.label = tk.Label(self.root, text='')
         self.canvas = tk.Canvas(
             self.root,
             width=self.WIDTH,
             height=self.HEIGHT,
             bg="lightblue"
             )
+        self.country_names = [
+    'afghanistan',
+    'albania',
+    'algeria',
+    'andorra',
+    'angola',
+    'antigua and barb.',
+    'argentina',
+    'armenia',
+    'australia',
+    'austria',
+    'azerbaijan',
+    'bahamas',
+    'bahrain',
+    'bangladesh',
+    'barbados',
+    'belarus',
+    'belgium',
+    'belize',
+    'benin',
+    'bhutan',
+    'bolivia',
+    'bosnia and herz.',
+    'botswana',
+    'brazil',
+    'brunei',
+    'bulgaria',
+    'burkina faso',
+    'burundi',
+    'cabo verde',
+    'cambodia',
+    'cameroon',
+    'canada',
+    'central african rep.',
+    'chad',
+    'chile',
+    'china',
+    'colombia',
+    'comoros',
+    'congo',
+    'costa rica',
+    "côte d'ivoire",
+    'croatia',
+    'cuba',
+    'cyprus',
+    'czechia',
+    'dem. rep. congo',
+    'denmark',
+    'djibouti',
+    'dominica',
+    'dominican rep.',
+    'ecuador',
+    'egypt',
+    'el salvador',
+    'eq. guinea',
+    'eritrea',
+    'estonia',
+    'eswatini',
+    'ethiopia',
+    'fiji',
+    'finland',
+    'france',
+    'gabon',
+    'gambia',
+    'georgia',
+    'germany',
+    'ghana',
+    'greece',
+    'grenada',
+    'guatemala',
+    'guinea',
+    'guinea-bissau',
+    'guyana',
+    'haiti',
+    'honduras',
+    'hungary',
+    'iceland',
+    'india',
+    'indonesia',
+    'iran',
+    'iraq',
+    'ireland',
+    'israel',
+    'italy',
+    'jamaica',
+    'japan',
+    'jordan',
+    'kazakhstan',
+    'kenya',
+    'kiribati',
+    'north korea',
+    'south korea',
+    'kosovo',
+    'kuwait',
+    'kyrgyzstan',
+    'laos',
+    'latvia',
+    'lebanon',
+    'lesotho',
+    'liberia',
+    'libya',
+    'liechtenstein',
+    'lithuania',
+    'luxembourg',
+    'madagascar',
+    'malawi',
+    'malaysia',
+    'maldives',
+    'mali',
+    'malta',
+    'marshall is.',
+    'mauritania',
+    'mauritius',
+    'mexico',
+    'micronesia',
+    'moldova',
+    'monaco',
+    'mongolia',
+    'montenegro',
+    'morocco',
+    'mozambique',
+    'myanmar',
+    'namibia',
+    'nauru',
+    'nepal',
+    'netherlands',
+    'new zealand',
+    'nicaragua',
+    'niger',
+    'nigeria',
+    'north macedonia',
+    'norway',
+    'oman',
+    'pakistan',
+    'palau',
+    'palestine',
+    'panama',
+    'papua new guinea',
+    'paraguay',
+    'peru',
+    'philippines',
+    'poland',
+    'portugal',
+    'qatar',
+    'romania',
+    'russia',
+    'rwanda',
+    'st. kitts and nevis',
+    'saint lucia',
+    'st. vin. and gren.',
+    'samoa',
+    'san marino',
+    'são tomé and principe',
+    'saudi arabia',
+    'senegal',
+    'serbia',
+    'seychelles',
+    'sierra leone',
+    'singapore',
+    'slovakia',
+    'slovenia',
+    'solomon is.',
+    'somalia',
+    'south africa',
+    's. sudan',
+    'spain',
+    'sri lanka',
+    'sudan',
+    'suriname',
+    'sweden',
+    'switzerland',
+    'syria',
+    'taiwan',
+    'tajikistan',
+    'tanzania',
+    'thailand',
+    'timor-leste',
+    'togo',
+    'tonga',
+    'trinidad and tobago',
+    'tunisia',
+    'turkey',
+    'turkmenistan',
+    'tuvalu',
+    'uganda',
+    'ukraine',
+    'united arab emirates',
+    'united kingdom',
+    'united states of america',
+    'uruguay',
+    'uzbekistan',
+    'vanuatu',
+    'vatican',
+    'venezuela',
+    'vietnam',
+    'yemen',
+    'zambia',
+    'zimbabwe']
 
     def main(self):
         self.canvas.pack()
         self.button.pack()
         self.entry.pack()
+        self.label.pack()
 
         #Load the JSON
         with open("game/Game_Files/cleaned_world.json", "r", encoding="utf-8") as f:
@@ -146,12 +347,35 @@ class gameMode1():
                     fill=color
                 )
     
+
     def on_button_click(self):
-        country_name = self.entry.get()
+        country_name = self.entry.get().lower()
 
-        self.fill_country(country_name, "red")
+        #Exact match
+        if country_name in self.country_names:
+            self.fill_country(country_name, "red")
+            self.label.config(text="")
 
-        self.entry.select_clear()
+        else:
+            matches = difflib.get_close_matches(
+                country_name,
+                self.country_names,
+                n=1,
+                cutoff=0.6
+            )
+
+            if matches:
+                corrected_name = matches[0]
+                self.label.config(
+                    text=f"Did you mean '{corrected_name}'?"
+                )
+
+            else:
+                self.label.config(
+                    text="Country not found"
+                )
+
+        self.entry.delete(0, tk.END)
 
 game_instance = gameMode1()
 game_instance.main()
